8dae521d83aef42ffb4771f706d0d55451c23c86
Hieu Phan <huyhieu.210198@gmail.com>
2019-11-07 00:27:16 -0500

    add webhook

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict'; 
+
+const 
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	app = express().use(bodyParser.json());
+
+
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+
+app.post('/webhook', (req, res) => {
+	let body = req.body;
+
+	if (body.object == 'page') {
+
+		body.entry.forEach(function(entry) {
+			let webhook_event = entry.messaging[0];
+			console.log(webhook_event);
+		});
+
+	res.status(200).send('EVENT_RECEIVED');
+
+	} else {
+
+		res.sendStatus(404);
+
+	}
+
+});
+
+
+app.get('webhook', (req, res) => {
+	let VERIFY_TOKEN = "test123";
+
+	let mode = req.query['hub.mode'];
+	let token = req.query['hub.verify_token'];
+	let challenge = req.query['hub.challenge'];
+
+	if (mode && token) {
+
+		if (mode == 'subscribe' && token == VERIFY_TOKEN) {
+
+			console.log('WEBHOOK_VERIFIED');
+			res.status(200).send(challenge);
+
+		} else {
+
+			res.sendStatus(403);
+
+		}
+
+	}
+});
